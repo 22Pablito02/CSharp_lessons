32a2c482a95c94578c03e751a9f8fc297a7bf2e2
Pablito <PavelVolkovPro@yandex.ru>
2022-09-02 23:06:46 +0300

    task 4

--- home_work_sem1/Program.cs
+++ home_work_sem1/Program.cs
@@ -1,4 +1,6 @@
-﻿int num1 = Convert.ToInt32(Console.ReadLine());
+﻿// Задача 2
+/* 
+int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1 > num2){
@@ -8,3 +10,26 @@ if (num1 > num2){
 }else{
     Console.WriteLine(num2);
 }
+*/
+
+// Задача 4
+/*
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (num1 > num2){
+    max = num1;
+}else{
+    max = num2;
+}
+
+if (max > num3){
+    Console.WriteLine("Максимальное число: " + max);
+}else{
+    max = num3;
+    Console.WriteLine("Максимальное число: " + max);
+}
+*/
