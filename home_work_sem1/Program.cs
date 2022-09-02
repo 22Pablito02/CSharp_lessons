7f991f73d19a9fae224ee9e30cef0fc4c583c52d
Pablito <PavelVolkovPro@yandex.ru>
2022-09-02 22:43:34 +0300

    task 2

--- home_work_sem1/Program.cs
+++ home_work_sem1/Program.cs
@@ -1,2 +1,10 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2){
+    Console.WriteLine(num1);
+}else if(num1 == num2){
+    Console.WriteLine("Числа равны");
+}else{
+    Console.WriteLine(num2);
+}
