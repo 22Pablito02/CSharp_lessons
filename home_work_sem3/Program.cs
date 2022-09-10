32baf42a54f203a108a0258b3067ca5d2aa80635
Pablito <PavelVolkovPro@yandex.ru>
2022-09-10 18:08:38 +0300

    task 19

--- home_work_sem3/Program.cs
+++ home_work_sem3/Program.cs
@@ -1,1 +1,19 @@
-﻿//Начнем
+﻿
+//Задача 19
+
+string number = Console.ReadLine();
+int i = 0;
+int j = number.Length - 1;
+
+while (i < number.Length){
+   
+    if (number[i] != number[j]){
+        Console.WriteLine("Введеное число не является палиндромом!");
+        break;
+    }else{
+        Console.WriteLine("Введеное число палиндром!");
+        break;
+    }
+    i++;
+    j--;
+}
