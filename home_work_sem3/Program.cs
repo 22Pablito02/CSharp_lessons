1c8db3a2fda674a3b2a77d68d83ce3c52398b399
Pablito <PavelVolkovPro@yandex.ru>
2022-09-10 18:35:50 +0300

    task 23

--- home_work_sem3/Program.cs
+++ home_work_sem3/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 //Задача 19
-
+/*
 string number = Console.ReadLine();
 int i = 0;
 int j = number.Length - 1;
@@ -17,3 +17,20 @@ while (i < number.Length){
     i++;
     j--;
 }
+*/
+
+
+// Задача 23
+
+int  N = Convert.ToInt32(Console.ReadLine());
+int cobe = 0;
+
+for(int i = 1; i <= N; i++){
+    
+    cobe = i;
+    Console.Write(Math.Pow(cobe, 3) + " ");
+}
+
+
+
+
