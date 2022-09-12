8f30756c789bf48de200b46453bd66c4d796ca49
Pablito <PavelVolkovPro@yandex.ru>
2022-09-12 21:36:37 +0300

    task 25

---
+++ home_work_sem4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 25
+
+int num = prompt("Введите число возводимое в степень: "); 
+int deg = prompt("Введите степень: ");
+Console.WriteLine(raising(num, deg));
+
+int prompt(string massage){
+    Console.Write(massage);
+    int answer = Convert.ToInt32(Console.ReadLine()); 
+    return answer; 
+}
+
+int raising(int number, int degree){
+    int result = 1;
+    for(int i = 1; i <= degree; i++){
+        result *=number;
+    }
+    return result;
+}
+
