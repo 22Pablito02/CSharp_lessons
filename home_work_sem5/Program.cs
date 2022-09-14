e5f2d02deffa43c3c2c7c7a1f70f1537a3a5bee7
Pablito <PavelVolkovPro@yandex.ru>
2022-09-14 12:02:59 +0300

    task 36

--- home_work_sem5/Program.cs
+++ home_work_sem5/Program.cs
@@ -1,8 +1,5 @@
 ﻿// Задача 34
-// Задайте массив заполненный случайными положительными 
-// трёхзначными числами.Напишите программу,
-// которая покажет количество чётных чисел в массиве.
-
+/*
 int quantity = prompt("Введите число элементов массива: ");
 int[] array = CreateArray(quantity);
 PrintArr(array);
@@ -42,4 +39,54 @@ int SearchEvenNumber(int[] arr){
     }
     return count;
 }
+*/
+
+
+// Задача 36: Найдите сумму элементов, стоящих на нечётных позициях.
+
+int quantity = prompt("Введите число элементов массива: ");
+int[] array = CreateArray(quantity);
+PrintArr(array);
+Console.WriteLine("");
+int result = SumOdd(array);
+Console.Write($"Сумма нечетных чисел в массиве: {result}");
+
+
+int prompt(string massage)
+{
+    Console.Write(massage);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int[] CreateArray(int size){
+    Random rnd = new Random();
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++){
+        arr[i] = rnd.Next(1, 10);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr){
+
+    for(int i = 0; i < arr.Length; i++){
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int SumOdd(int[] arr){
+    int count = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(i % 2 != 1){
+            count += arr[i];
+        }
+    }
+    return count;
+}
+
+
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
