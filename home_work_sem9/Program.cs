75e7e0e4982354f6797e6c3fc2b158ceed077f4b
Pablito <PavelVolkovPro@yandex.ru>
2022-10-02 18:31:35 +0300

    task 68

--- home_work_sem9/Program.cs
+++ home_work_sem9/Program.cs
@@ -22,7 +22,7 @@ int prompt(string massege)
 
 
 // Задача 66
-
+/*
 int StarttNum = prompt("Введите число N: ") + 1;
 int FinishNum = prompt("Введите число M: ");
 int count = StarttNum;
@@ -43,12 +43,41 @@ int IntervalSum(int SNum, int FNum, int count)
     return SNum + IntervalSum(SNum + 1, FNum, count);
 
 }
-
-
-
     int prompt(string massege)
     {
         Console.Write(massege);
         int answer = Convert.ToInt32(Console.ReadLine());
         return answer;
     }
+
+    */
+
+// Задача 68
+
+int Num_M = prompt("Введите число M: ");
+int Num_N = prompt("Введите число N: ");
+Console.Write(akkerman(Num_M, Num_N));
+
+int akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (akkerman(m - 1, akkerman(m, n - 1)));
+    }
+}
+
+
+int prompt(string massege)
+{
+    Console.Write(massege);
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
