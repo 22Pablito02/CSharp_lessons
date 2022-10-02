bd5697fb2713e5a48a424f84c62969ebff9abe1c
Pablito <PavelVolkovPro@yandex.ru>
2022-10-02 18:19:53 +0300

    task 66

--- home_work_sem9/Program.cs
+++ home_work_sem9/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задание 64
-
+/*
 int num = prompt("Введите число: ");
  Console.Write(ReversCounter(num));
 
@@ -18,3 +18,37 @@ int prompt(string massege)
     int answer = Convert.ToInt32(Console.ReadLine());
     return answer;
 }
+*/
+
+
+// Задача 66
+
+int StarttNum = prompt("Введите число N: ") + 1;
+int FinishNum = prompt("Введите число M: ");
+int count = StarttNum;
+
+Console.Write(IntervalSum(StarttNum, FinishNum, count));
+int IntervalSum(int SNum, int FNum, int count)
+{
+    count++;
+    if (StarttNum > FinishNum)
+    {
+        return 0;
+    }
+    else if (FinishNum == 0)
+    {
+        return 0;
+    }
+    else if (count == FNum) return SNum;
+    return SNum + IntervalSum(SNum + 1, FNum, count);
+
+}
+
+
+
+    int prompt(string massege)
+    {
+        Console.Write(massege);
+        int answer = Convert.ToInt32(Console.ReadLine());
+        return answer;
+    }
